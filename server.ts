1c3d05c66d2fbc8ff49d0e97188b41769c28d0de
Nicholas <berubenic@gmail.com>
2023-09-16 10:38:47 -0400

    Modify server.ts Check if running on Passenger, if so, do not specify a port.

--- server.ts
+++ server.ts
@@ -36,11 +36,23 @@ export function app(): express.Express {
   return server;
 }
 
-function run(): void {
-  const port = process.env['PORT'] || 4000;
+function isRunningOnApachePassenger(): boolean {
+  return moduleFilename.includes('lsnode.js');
+}
 
+function run(): void {
   // Start up the Node server
   const server = app();
+
+  if (isRunningOnApachePassenger()) {
+    server.listen(() => {
+      console.log('Node Express listening to Passenger Apache');
+    });
+    return;
+  }
+
+  const port = process.env['PORT'] || 4000;
+
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
@@ -52,7 +64,11 @@ function run(): void {
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = mainModule && mainModule.filename || '';
-if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+if (
+  moduleFilename === __filename ||
+  moduleFilename.includes('iisnode') ||
+  isRunningOnApachePassenger()
+   ) {
   run();
 }
 
